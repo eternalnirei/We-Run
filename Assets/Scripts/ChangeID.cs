1c32bf546a540494c86fd6acd6a140a25acdfe2f
Astradeni <32953671+Astradeni@users.noreply.github.com>
2019-12-08 02:27:24 +0100

    The first project commit

---
+++ Assets/Scripts/ChangeID.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeID : MonoBehaviour
+{
+    public Material calm;
+    public Material excited;
+    public Material psychotic;
+    public enum states {Calm, Excited, Psychotic};
+    float timeToSwitch;
+    int randSwitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToSwitch = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+            renderingMaterial();
+        if (timeToSwitch <= 0)
+        {
+            renderingMaterial();
+        }
+        timeToSwitch -= Time.deltaTime;
+        Debug.Log(timeToSwitch);
+    }
+
+    void renderingMaterial()
+    {
+        //getiing the renderer component in order to use the materials
+        Renderer rend = GetComponent<Renderer>();
+        randSwitch = Random.Range(0, 3);
+        switch (randSwitch)
+        {
+            case 0:
+                rend.material = calm;
+                break;
+            case 1:
+                rend.material = excited;
+                break;
+            case 2:
+                rend.material = psychotic;
+                break;
+        }
+        //resetting timer
+        timeToSwitch = 3;
+    }
+}
