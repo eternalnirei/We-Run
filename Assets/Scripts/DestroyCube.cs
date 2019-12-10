b23ba4ccffc54449edbfd554ee9de8f2eb1b1dd9
Sara <kirsche@kindschus.de>
2019-12-10 21:09:53 +0100

    this is another try for adding

---
+++ Assets/Scripts/DestroyCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(10f);
+        Destroy(gameObject);
+    }
+
+   
+}
